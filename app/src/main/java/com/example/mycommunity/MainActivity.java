1f971d735440883afc245227f87c012bc8277340
khs <kohs116@naver.com>
2021-11-14 15:36:20 +0900

    community updates

---
+++ app/src/main/java/com/example/mycommunity/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.mycommunity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button imageButton1 = (Button) findViewById(R.id.button1);
+        Button imageButton2 = (Button) findViewById(R.id.button2);
+        Button imageButton3 = (Button) findViewById(R.id.button3);
+        Button imageButton4 = (Button) findViewById(R.id.button4);
+
+        Button imageButton5 = (Button) findViewById(R.id.reg_button1);
+        Button imageButton6 = (Button) findViewById(R.id.reg_button2);
+        Button imageButton7 = (Button) findViewById(R.id.reg_button3);
+        Button imageButton8 = (Button) findViewById(R.id.reg_button4);
+
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
+        imageButton3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
+        imageButton4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        imageButton5.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
+        imageButton6.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
+        imageButton7.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
+        imageButton8.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mycommunity/SubActivity.java
@@ -0,0 +1,23 @@
+package com.example.mycommunity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_list1);
+        setContentView(R.layout.activity_list2);
+        setContentView(R.layout.activity_list3);
+        setContentView(R.layout.activity_list4);
+
+        setContentView(R.layout.activity_register1);
+        setContentView(R.layout.activity_register2);
+        setContentView(R.layout.activity_register3);
+        setContentView(R.layout.activity_register4);
+    }
+}
